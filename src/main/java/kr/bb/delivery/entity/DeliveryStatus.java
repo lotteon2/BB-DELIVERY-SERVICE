c36b0cdda051025eab53a4977dbc7972a038cb27
binarywoo27 <blu_mountain@naver.com>
2023-11-29 15:55:00 +0900

    :bug: 배송 상태 Enum 클래스 상수값 변경 (논의완료)

--- src/main/java/kr/bb/delivery/entity/DeliveryStatus.java
+++ src/main/java/kr/bb/delivery/entity/DeliveryStatus.java
@@ -1,10 +1,10 @@
 package kr.bb.delivery.entity;
 
 public enum DeliveryStatus {
-  DELIVERY_PENDING("주문접수", 1),
-  DELIVERY_PROCESSING("배송시작", 2),
-  DELIVERY_COMPLETED("배송완료", 3),
-  DELIVERY_CANCELED("배송 취소", 4);
+  PENDING("주문접수", 1),
+  PROCESSING("배송시작", 2),
+  COMPLETED("배송완료", 3),
+  CANCELED("배송 취소", 4);
 
   private final String message;
   private final int order;
