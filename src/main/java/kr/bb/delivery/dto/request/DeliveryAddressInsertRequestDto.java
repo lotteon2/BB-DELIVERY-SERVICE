4a60f90c0639a1403c76e816a76b4af486b80a32
binarywoo27 <blu_mountain@naver.com>
2023-12-22 09:49:40 +0900

    :safety_vest: Validation 추가

--- src/main/java/kr/bb/delivery/dto/request/DeliveryAddressInsertRequestDto.java
+++ src/main/java/kr/bb/delivery/dto/request/DeliveryAddressInsertRequestDto.java
@@ -1,5 +1,8 @@
 package kr.bb.delivery.dto.request;
 
+import java.time.LocalDateTime;
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotEmpty;
 import kr.bb.delivery.entity.DeliveryAddress;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,23 +14,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DeliveryAddressInsertRequestDto {
-    private Long deliveryAddressId;
-    private Long userId;
-    private String recipientName;
-    private String zipcode;
-    private String roadName;
-    private String addressDetail;
-    private String phoneNumber;
+  @Nullable private Long deliveryAddressId;
+  @NotEmpty private Long userId;
+  @NotEmpty private String recipientName;
+  @NotEmpty private String zipcode;
+  @NotEmpty private String roadName;
+  @NotEmpty private String addressDetail;
+  @NotEmpty private String phoneNumber;
 
-    public DeliveryAddress toEntity(){
-        return DeliveryAddress.builder()
-                .userId(this.getUserId())
-                .deliveryRecipientName(this.getRecipientName())
-                .deliveryRoadName(this.getRoadName())
-                .deliveryAddressDetail(this.getAddressDetail())
-                .deliveryZipcode(this.getZipcode())
-                .deliveryRecipientPhoneNumber(this.getPhoneNumber())
-                .build();
-    }
+  public DeliveryAddress toEntity() {
+    return DeliveryAddress.builder()
+        .userId(this.getUserId())
+        .deliveryRecipientName(this.getRecipientName())
+        .deliveryRoadName(this.getRoadName())
+        .deliveryAddressDetail(this.getAddressDetail())
+        .deliveryZipcode(this.getZipcode())
+        .deliveryRecipientPhoneNumber(this.getPhoneNumber())
+        .deliveryUsedAt(LocalDateTime.now())
+        .build();
+  }
 }
-
